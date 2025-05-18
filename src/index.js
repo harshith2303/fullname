eea40076de0dd2cf0277fc7c12f51c056ba5f519
harshith2303 <harshithe00@gmail.com>
2025-05-18 16:35:27 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import { useState } from "react";
+
+function App() {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [submittedName, setSubmittedName] = useState("");
+
+  const firstNameChange = (e) => {
+    setFirstname(e.target.value);
+  };
+
+  const lastNameChange = (e) => {
+    setLastname(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedName(`${firstname} ${lastname}`);
+  };
+
+  return (
+    <div>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstname">First Name:</label>
+        <input
+          type="text"
+          id="firstname"
+          onChange={firstNameChange}
+          value={firstname}
+        />
+        <br />
+        <label htmlFor="lastname">Last Name:</label>
+        <input
+          type="text"
+          id="lastname"
+          onChange={lastNameChange}
+          value={lastname}
+        />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+
+      {submittedName && (
+        <h4 style={{ marginTop: "20px" }}>Full Name: {submittedName}</h4>
+      )}
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +58,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
